df5d9d387ba621ec47af1e90124f61f2c702f514
Imre Vari <imrevari2@gmail.com>
2023-11-16 21:51:43 -0500

    sorting working properly

--- src/components/admin/AllTicketsSortableTable.tsx
+++ src/components/admin/AllTicketsSortableTable.tsx
@@ -4,7 +4,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { LotteryTicket } from '../../interfaces/interfaces';
 import { UserType } from '../../interfaces/enums';
 import { TableSortLabel } from '@mui/material';
@@ -14,7 +14,73 @@ interface TableProps {
     rows: LotteryTicket[]
 }
 
+interface IsAscending{
+    purchased: boolean;
+    selectedNumbers: boolean;
+    userType: boolean;
+    drawConducted: boolean;
+    winningNumbers: boolean;
+    amountWon: boolean;
+}
+
+type Rows = 'purchased' | 'selectedNumbers' | 'userType' | 'drawConducted' | 'winningNumbers' | 'amountWon';
+
 const AllTicketsSortableTable: FC<TableProps> = ({rows}) => {
+
+
+    const [sortedRows, setSortedRows] = useState<LotteryTicket[]>(rows)
+
+    const [isAscending, setIsAscending] = useState<IsAscending>({
+        purchased: false,
+        selectedNumbers: false,
+        userType: false,
+        drawConducted: false,
+        winningNumbers: false,
+        amountWon: false,
+    })
+
+    useEffect(() => {setSortedRows(rows)}, [rows])
+
+    const sorting = (row: Rows) => {
+        const stateCopy = {...isAscending}
+        const isRowAsc = stateCopy[row]
+        setIsAscending({...stateCopy, [row]: !isRowAsc})
+        sortRows(row)
+    }
+
+    const sortRows = (row: Rows) => {
+        const rowClone = [...sortedRows];
+        switch (row) {
+            case 'winningNumbers':
+                rowClone.sort(({winningNumbers: a}, {winningNumbers: b} ) => 
+                isAscending[row] ?
+                (b ? b.length : 0) - (a ? a.length : 0)
+                :
+                (a ? a.length : 0) - (b ? b.length : 0))
+                break;
+            case 'userType': 
+                rowClone.sort(({user:{userType: a}}, {user:{userType: b}} ) => 
+                isAscending[row] ? a - b : b - a)
+                break;
+            case 'drawConducted':
+                rowClone.sort(({drawConducted: a}, {drawConducted: b} ) => 
+                isAscending[row] ? ((a === b)? 0 : a? -1 : 1) : ((a === b)? 0 : a? 1 : -1))
+                break;
+            case 'amountWon':
+                rowClone.sort(({amountWon: a}, {amountWon: b} ) => 
+                isAscending[row] ? ((a ?? 0) - (b ?? 0)) : ((b ?? 0) - (a ?? 0)))
+                break;
+            case 'purchased':
+                rowClone.sort(({purchased: a}, {purchased: b} ) => 
+                isAscending[row] ? (a.getTime() - b.getTime()) : (b.getTime() - a.getTime()))
+                break;
+        }
+        setSortedRows(rowClone)
+    }
+
+
+
+
   return (
     <TableContainer sx={{marginTop: '10px'}}>
       <Table sx={{ minWidth: '850px', maxWidth: '100%'}} aria-label="simple table">
@@ -25,26 +91,26 @@ const AllTicketsSortableTable: FC<TableProps> = ({rows}) => {
                 <TableSortLabel
                     id={'purchased'}
                     active={true}
-                    direction={'desc'}
-                    onClick={(e) => console.log('purchased')}
+                    direction={isAscending['purchased'] ? 'asc' : 'desc'}
+                    onClick={() => sorting('purchased')}
                 />
             </TableCell>
             <TableCell >
                 {`selected numbers`}
-                <TableSortLabel
+                {/* <TableSortLabel
                     id={'selectedNumbers'}
                     active={true}
-                    direction={'desc'}
-                    onClick={(e) => console.log('selectedNumbers')}
-                />
+                    direction={isAscending['selectedNumbers'] ? 'asc' : 'desc'}
+                    onClick={() => sorting('selectedNumbers')}
+                /> */}
             </TableCell>
             <TableCell >
                 {`Purchased by`}
                 <TableSortLabel
                     id={'userType'}
                     active={true}
-                    direction={'desc'}
-                    onClick={(e) => console.log('userType')}
+                    direction={isAscending['userType'] ? 'asc' : 'desc'}
+                    onClick={() => sorting('userType')}
                 />
             </TableCell>
             <TableCell >
@@ -52,32 +118,32 @@ const AllTicketsSortableTable: FC<TableProps> = ({rows}) => {
                 <TableSortLabel
                     id={'drawConducted'}
                     active={true}
-                    direction={'desc'}
-                    onClick={(e) => console.log('drawConducted')}
+                    direction={isAscending['drawConducted'] ? 'asc' : 'desc'}
+                    onClick={() => sorting('drawConducted')}
                 />
             </TableCell>
             <TableCell >
                 {`Successful guesses`}
                 <TableSortLabel
-                    id={'purchased'}
+                    id={'winningNumbers'}
                     active={true}
-                    direction={'desc'}
-                    onClick={(e) => console.log('purchased')}
+                    direction={isAscending['winningNumbers'] ? 'asc' : 'desc'}
+                    onClick={() => sorting('winningNumbers')}
                 />
             </TableCell>
             <TableCell >
                 {`Amount won`}
                 <TableSortLabel
-                    id={'purchased'}
+                    id={'amountWon'}
                     active={true}
-                    direction={'desc'}
-                    onClick={(e) => console.log('purchased')}
+                    direction={isAscending['amountWon'] ? 'asc' : 'desc'}
+                    onClick={() => sorting('amountWon')}
                 />
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map(({purchased, selectedNumbers, user:{userType}, drawConducted, amountWon, winningNumbers}, index) => (
+          {sortedRows.map(({purchased, selectedNumbers, user:{userType}, drawConducted, amountWon, winningNumbers}, index) => (
             <TableRow
               key={index}
             >
