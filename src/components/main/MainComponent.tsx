3eb4cd83e49dc29581ae4a6ddaa17f812841cc46
Imre Vari <imrevari2@gmail.com>
2023-11-16 22:59:03 -0500

    final version (some refactoring might be useful)

--- src/components/main/MainComponent.tsx
+++ src/components/main/MainComponent.tsx
@@ -9,51 +9,62 @@ import { HEADER } from '../../interfaces/constants';
 import AdminsPage from '../admin/AdminsPage';
 import PlayersPage from '../player/PlayersPage';
 import { useStateContext } from '../../stateContext/StateContext';
+import Questions from '../questions/Questions';
 
 const MainComponent: FC<any> = () => {
 
-    const [isLandingPage, setIsLandingPage] = useState<boolean>(false)
+    const [isLandingPage, setIsLandingPage] = useState<boolean>(true)
     const [isAdmin, setIsAdmin] = useState<boolean>(false)
 
     const {resetGame, resetRound} = useStateContext()
 
+    const openAdminPage = () => {
+        setIsLandingPage(false)
+        setIsAdmin(true)
+    }
+
+    const openPlayersPage = () => {
+        setIsLandingPage(false)
+        setIsAdmin(false)
+    }
+
     
     return(
         <>
-        <Box>
-        <AppBar position="static">
-            <Toolbar>
-            
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left'}}>
-                    {HEADER}
-                </Typography>
-                <Button color="inherit" style={{maxWidth: '130px', minWidth: '130px'}} 
-                    onClick={() => setIsAdmin(false)}>Player</Button>
-                <Button color="inherit" style={{maxWidth: '130px', minWidth: '130px'}} 
-                    onClick={() => setIsAdmin(true)}>Admin</Button>
+            <Box>
+                <AppBar position="static">
+                    <Toolbar>
+                    
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left'}}>
+                            {HEADER}
+                        </Typography>
+                        <Button color="inherit" style={{maxWidth: '130px', minWidth: '130px'}} 
+                            onClick={openPlayersPage}>Player</Button>
+                        <Button color="inherit" style={{maxWidth: '130px', minWidth: '130px'}} 
+                            onClick={openAdminPage}>Admin</Button>
 
-                <Button color="inherit" 
-                    style={{maxWidth: '130px', minWidth: '130px'}}
-                    onClick={() => resetRound()}>Reset round</Button>
-                <Button color="inherit" 
-                    style={{maxWidth: '130px', minWidth: '130px'}}
-                    onClick={() => resetGame()}
-                    >Reset game</Button>
-                <Button color="inherit" 
-                    style={{maxWidth: '130px', minWidth: '130px'}}
-                    onClick={() => resetGame()}
-                    >My questions</Button>
-            </Toolbar>
-        </AppBar>
-        </Box>
-        <br></br>
-
-        <Container >
-            {isAdmin ? <AdminsPage /> : <PlayersPage />}
-            
-            
-        </Container>
-    
+                        <Button color="inherit" 
+                            style={{maxWidth: '130px', minWidth: '130px'}}
+                            onClick={() => resetRound()}>Reset round</Button>
+                        <Button color="inherit" 
+                            style={{maxWidth: '130px', minWidth: '130px'}}
+                            onClick={() => resetGame()}
+                            >Reset game</Button>
+                        <Button color="inherit" 
+                            style={{maxWidth: '130px', minWidth: '130px'}}
+                            onClick={() => setIsLandingPage(true)}
+                            >My questions</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <br></br>
+            <Container >
+                {isLandingPage
+                ?
+                <Questions />
+                :
+                isAdmin ? <AdminsPage /> : <PlayersPage />}
+            </Container>
         </>)
 }
 
---
+++ src/components/questions/Questions.tsx
@@ -0,0 +1,29 @@
+import Box from '@mui/material/Box';
+import { FC } from 'react';
+
+const Questions: FC<any> = () => {
+
+    return( 
+        <Box>
+            <h2>K: Ne legyen autentikáció. Akkor legyen két endpoint (útvonal) a játékosnak és a játékvezetőnek?</h2>
+            <h3>V: Eleinte használtam Routert, de utána "single page"-ként készitettem el. A játékosi és a tulaj felületek a megfelelő gomb megnyomásával érhetők el.</h3>
+
+            <h2>K: Illetve akkor csak egy játékos legyen?</h2>
+            <h3>V: Végül egy lett ;)</h3>
+
+            <h2>K: Az elmentett nevet, pénzt, stb elég pl a local storage-ben elmenteni, vagy legyen api hivás és kell egy kis pl node.js backend</h2>
+            <h3>V: Végül nincs backend, minden ammi hosszú távú az a local storage-ban van elmentve.</h3>
+
+            
+            <h2>K: Új kört indíthat. Ilyenkor a leadott szelvények törlődjenek?</h2>
+            <h3>V: Törlődtek. Csak a név és az összegek maradnak meg.</h3>
+
+            <h2>K: Volr egy ilyen elvárás: Az egyes találatokra szelvényenként kifizetendő nyeremény. Ez mit is jelent?</h2>
+            <h3>V: Nem sikerült kitalálnom, ezért szerintem nincs ilyen infó, de megnézhetitek.</h3>
+        </Box>
+        )
+}
+
+Questions.displayName = 'Questions';
+
+export default Questions;
